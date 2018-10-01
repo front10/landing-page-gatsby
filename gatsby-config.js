a02f29144ec3423068821f98a3e1275e67b99ce1
Jorge Luis Suárez Falcón <jlfalcon3589@gmail.com>
2018-10-01 10:32:32 -0400

    Added favicon to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  pathPrefix: `/landing-page-gatsby`
+  pathPrefix: `/landing-page-gatsby`,
+  plugins: [
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: true,
+          favicons: true,
+          firefox: true,
+          opengraph: true,
+          twitter: true,
+          yandex: true,
+          windows: true
+        }
+      }
+    }
+  ]
 };
+
