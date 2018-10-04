7ec326f7dab8b86142f0887bbee020ff69c728c4
Yulier Camilo Garrido Garrido <ycgarrido@gmail.com>
2018-10-04 15:59:12 -0400

    Validate polyfill

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,7 +69,9 @@ export default () => (
   <div className="App">
     <Helmet defaultTitle={`Landing Page Book`}>
       <link rel="shortcut icon" href={logo} />
-      <script src="https://cdn.polyfill.io/v2/polyfill.js?features=default,Symbol"></script>
+      {(false || !!document.documentMode) && (
+        <script src="https://cdn.polyfill.io/v2/polyfill.js?features=default,Symbol" />
+      )}
     </Helmet>
     <Analytics idTracking="UA-44521541-9" />
     <Hero opacity={1} overlayColor="#212529" particlesParams={particles}>
