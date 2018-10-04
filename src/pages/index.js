216ed7d9cf6ce8a688ff6080ab5b6bcf5fb914d7
Jorge Luis Suárez Falcón <jlfalcon3589@gmail.com>
2018-10-04 13:23:38 -0400

    Adding polyfill library for compatibility with IE

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,6 +69,7 @@ export default () => (
   <div className="App">
     <Helmet defaultTitle={`Landing Page Book`}>
       <link rel="shortcut icon" href={logo} />
+      <script src="https://cdn.polyfill.io/v2/polyfill.js?features=default,Symbol"></script>
     </Helmet>
     <Analytics idTracking="UA-44521541-9" />
     <Hero opacity={1} overlayColor="#212529" particlesParams={particles}>
